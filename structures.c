ecd7445169c251bf8e34e92f5b1f6d1dfe00ccdb
David L Parkhurst <dlpark@usgs.gov>
2006-01-26 16:13:02 +0000

    Modified with Tony's Donnan model.
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc/trunk/src@736 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- structures.c
+++ structures.c
@@ -4921,6 +4921,9 @@ int surface_init(struct surface *surface_ptr, int n_user, int n_user_end, char *
         surface_ptr->diffuse_layer = FALSE;
         surface_ptr->edl = TRUE;
 	surface_ptr->only_counter_ions = FALSE;
+	surface_ptr->donnan = FALSE;
+	surface_ptr->debye_units = 0.0;
+	surface_ptr->transport = FALSE;
 	surface_ptr->thickness = 1e-8;
 	surface_ptr->solution_equilibria = FALSE;
 	surface_ptr->n_solution = -999;
