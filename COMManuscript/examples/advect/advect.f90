886caf9ec2b3c60facd6b64f8acbb2090dc34b2d
David L Parkhurst <dlpark@usgs.gov>
2010-03-23 23:11:41 +0000

    Working version with all the right methods as of now.
    Think I'll use an include or module to shorten.
    
    Will need to add Create and Destroy.
    
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc/trunk@4188 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- COMManuscript/examples/advect/advect.f90
+++ COMManuscript/examples/advect/advect.f90
@@ -1,37 +1,36 @@
 program Advect
   include "IPhreeqc.f90.inc"
-  INTERFACE
-    SUBROUTINE ExtractWrite(i, vtype, dv, sv)
-      INTEGER,                         INTENT(IN)    :: i
-      INTEGER(KIND=4),  DIMENSION(:), INTENT(INOUT) :: vtype
-      REAL(KIND=8),     DIMENSION(:), INTENT(INOUT) :: dv
-      CHARACTER(LEN=*), DIMENSION(:), INTENT(INOUT) :: sv   
-    END SUBROUTINE
-  END INTERFACE
+  interface
+    subroutine extractwrite(i, vtype, dv, sv)
+      integer (kind=4),               intent(in)    :: i
+      integer (kind=4), dimension(:), intent(inout) :: vtype
+      real    (kind=8), dimension(:), intent(inout) :: dv
+      character(len=*), dimension(:), intent(inout) :: sv   
+    end subroutine
+  end interface
   character(len=1024) Istring
   integer,            dimension(7) :: vtype
+  real      (kind=8), dimension(7) :: dvalue
   character(len=100), dimension(7) :: svalue
-  real(kind=8),       dimension(7) :: dvalue
-! Load database, define initial conditions and selected output
-  if (LoadDatabase("phreeqc.dat") .ne. 0) call ErrorHandler()
-  If (RunFile("ic",.TRUE.,.FALSE.,.FALSE.,.FALSE.) .ne. 0) call ErrorHandler()
-! Run cell 1, extract/write result
-  Ierr = AccumulateLine("USE solution 1; USE equilibrium_phases 1; SAVE solution 1; END")
-  If (Run(.TRUE.,.FALSE.,.FALSE.,.FALSE.) .ne. 0) call ErrorHandler()
-  !if (RunString("RUN_CELLS; -cells; 1; END") .ne. 0) call ErrorHandler()
+
+!Load database, define initial conditions and selected output
+  if (LoadDatabase("phreeqc.dat") < 0) call ErrorHandler()
+  If (RunFile("ic") < 0) call ErrorHandler()
+
+!Run cell 1, extract/write result
+  if (RunString("RUN_CELLS; -cells; 1; END") < 0) call ErrorHandler()
   call ExtractWrite(1, vtype, dvalue, svalue)
-! Transfer cell 1 solution to cell 2,run cell 2, extract/write results
-!  Ierr = AccumulateLine("SOLUTION_MODIFY 2")    
-!  Ierr = AccumulateLine("   -cb      " // svalue(1))
-!  Ierr = AccumulateLine("   -total_h " // svalue(2))
-!  Ierr = AccumulateLine("   -total_o " // svalue(3))
-!  Ierr = AccumulateLine("   -totals ")
-!  Ierr = AccumulateLine("      C     " // svalue(4))
-!  Ierr = AccumulateLine("      Ca    " // svalue(5))
-!  Ierr = AccumulateLine("RUN_CELLS; -cells; 2; END")
-  Ierr = AccumulateLine("COPY solution 1 2; END")
-  Ierr = AccumulateLine("USE solution 2; USE equilibrium_phases 2; END")
-  if (Run(.true.,.FALSE.,.FALSE.,.FALSE.) .ne. 0) call ErrorHandler()
+
+!Transfer cell 1 solution to cell 2,run cell 2, extract/write results
+  Ierr = AccumulateLine("SOLUTION_MODIFY 2")    
+  Ierr = AccumulateLine("   -cb      " // svalue(1))
+  Ierr = AccumulateLine("   -total_h " // svalue(2))
+  Ierr = AccumulateLine("   -total_o " // svalue(3))
+  Ierr = AccumulateLine("   -totals  ")
+  Ierr = AccumulateLine("      C     " // svalue(4))
+  Ierr = AccumulateLine("      Ca    " // svalue(5))
+  Ierr = AccumulateLine("RUN_CELLS; -cells; 2; END")
+  if (RunAccumulated() < 0) call ErrorHandler()
   call ExtractWrite(2, vtype, dvalue, svalue)
 end program Advect
 
@@ -42,12 +41,13 @@ end subroutine ErrorHandler
 
 subroutine ExtractWrite(i, vtype, dv, sv)
   include "IPhreeqc.f90.inc"
-  integer,      dimension(:) :: vtype
-  real(kind=8), dimension(:) :: dv  
-  character(*), dimension(:) :: sv
+  integer (kind=4),               intent(in)    :: i
+  integer (kind=4), dimension(:), intent(inout) :: vtype
+  real    (kind=8), dimension(:), intent(inout) :: dv
+  character(len=*), dimension(:), intent(inout) :: sv    
   do j = 1, 7
-    Iresult = GetSelectedOutputValue(1, j, vtype(j), dv(j), sv(j))
-    if (vtype(j) .eq. 3) write(sv(j),"(1pE21.14)") dv(j)
+    ! Headings are on row 0
+    if (GetSelectedOutputValue(1,j,vtype(j),dv(j),sv(j)) < 0) call ErrorHandler()
   enddo
   write(*,"(a,i2/2(5x,a,f7.2))") "Cell",i,"pH:",dv(6),"SR(calcite):",dv(7)
 end subroutine ExtractWrite
