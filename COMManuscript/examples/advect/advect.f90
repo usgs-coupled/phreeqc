2b11fb784dcb0fc58c941a5c6d636e89a4291c51
David L Parkhurst <dlpark@usgs.gov>
2010-03-24 19:53:51 +0000

    Works with module. Still need to fix for instances. May remove error handling for simplicity.
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc/trunk@4190 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- COMManuscript/examples/advect/advect.f90
+++ COMManuscript/examples/advect/advect.f90
@@ -1,53 +1,51 @@
+module Subs
+  integer    (kind=4), dimension(7) :: vt
+  real       (kind=8), dimension(7) :: dv
+  character (len=100), dimension(7) :: sv 
+  contains
+  subroutine ExtractWrite(cell)
+    include "IPhreeqc.f90.inc"
+    integer    (kind=4), intent(in) :: cell
+    do j = 1, 7
+      ! Headings are on row 0
+      if(GetSelectedOutputValue(1,j,vt(j),dv(j),sv(j)).ne.VR_OK) call EHandler()
+    enddo
+    write(*,"(a,i2/2(5x,a,f7.2))") "Cell",cell,"pH:",dv(6),"SR(calcite):",dv(7) 
+  end subroutine ExtractWrite
+  subroutine EHandler()
+    include "IPhreeqc.f90.inc"
+!    integer i
+!    character(len=100) errstr
+!    do i = 1, GetErrorLineCount()
+!       call GetErrorLine(i, errstr)
+!       write (*,*) trim(errstr)
+!    enddo
+     call OutputLastError()
+    stop
+  end subroutine EHandler    
+end module Subs
 program Advect
+  use Subs
   include "IPhreeqc.f90.inc"
-  interface
-    subroutine extractwrite(i, vtype, dv, sv)
-      integer (kind=4),               intent(in)    :: i
-      integer (kind=4), dimension(:), intent(inout) :: vtype
-      real    (kind=8), dimension(:), intent(inout) :: dv
-      character(len=*), dimension(:), intent(inout) :: sv   
-    end subroutine
-  end interface
   character(len=1024) Istring
-  integer,            dimension(7) :: vtype
-  real      (kind=8), dimension(7) :: dvalue
-  character(len=100), dimension(7) :: svalue
-
+  
 !Load database, define initial conditions and selected output
-  if (LoadDatabase("phreeqc.dat") < 0) call ErrorHandler()
-  If (RunFile("ic") < 0) call ErrorHandler()
+  if (LoadDatabase("phreeqc.dat") .ne. 0) call EHandler()
+  If (RunFile("ic") .ne. 0) call EHandler()
 
 !Run cell 1, extract/write result
-  if (RunString("RUN_CELLS; -cells; 1; END") < 0) call ErrorHandler()
-  call ExtractWrite(1, vtype, dvalue, svalue)
+  if (RunString("RUN_CELLS; -cells; 1; END") .ne. 0) call EHandler()
+  call ExtractWrite(1)
 
-!Transfer cell 1 solution to cell 2,run cell 2, extract/write results
+!Advect cell 1 solution to cell 2, run cell 2, extract/write results
   Ierr = AccumulateLine("SOLUTION_MODIFY 2")    
-  Ierr = AccumulateLine("   -cb      " // svalue(1))
-  Ierr = AccumulateLine("   -total_h " // svalue(2))
-  Ierr = AccumulateLine("   -total_o " // svalue(3))
-  Ierr = AccumulateLine("   -totals  ")
-  Ierr = AccumulateLine("      C     " // svalue(4))
-  Ierr = AccumulateLine("      Ca    " // svalue(5))
+  Ierr = AccumulateLine("   -cb      " // sv(1))
+  Ierr = AccumulateLine("   -total_h " // sv(2))
+  Ierr = AccumulateLine("   -total_o " // sv(3))
+  Ierr = AccumulateLine("   -xtotals  ")
+  Ierr = AccumulateLine("      C     " // sv(4))
+  Ierr = AccumulateLine("      Ca    " // sv(5))
   Ierr = AccumulateLine("RUN_CELLS; -cells; 2; END")
-  if (RunAccumulated() < 0) call ErrorHandler()
-  call ExtractWrite(2, vtype, dvalue, svalue)
+  if (RunAccumulated() .ne. 0) call EHandler()
+  call ExtractWrite(2)
 end program Advect
-
-subroutine ErrorHandler()
-  call OutputLastError()
-  stop
-end subroutine ErrorHandler
-
-subroutine ExtractWrite(i, vtype, dv, sv)
-  include "IPhreeqc.f90.inc"
-  integer (kind=4),               intent(in)    :: i
-  integer (kind=4), dimension(:), intent(inout) :: vtype
-  real    (kind=8), dimension(:), intent(inout) :: dv
-  character(len=*), dimension(:), intent(inout) :: sv    
-  do j = 1, 7
-    ! Headings are on row 0
-    if (GetSelectedOutputValue(1,j,vtype(j),dv(j),sv(j)) < 0) call ErrorHandler()
-  enddo
-  write(*,"(a,i2/2(5x,a,f7.2))") "Cell",i,"pH:",dv(6),"SR(calcite):",dv(7)
-end subroutine ExtractWrite
