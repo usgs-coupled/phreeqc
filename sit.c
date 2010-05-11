767e802a5612eaad7c768ccd7c36def4811cbfa5
David L Parkhurst <dlpark@usgs.gov>
2010-05-11 16:48:32 +0000

    Removed the connection between remove_unstable_phases and the sit and pitzer versions of remove_unstable_phases.
    Looks like what I did was just wrong. I think the variables needed to be defined correctly for the class version, but there should be no interaction with the original remove_unstable_phases.
    
    I will run test cases.
    
    Hopefully this will fix Kinniburgh's problem.
    
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc/trunk/src@4376 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- sit.c
+++ sit.c
@@ -1099,7 +1099,7 @@ model_sit(void)
 			stop_program = TRUE;
 			break;
 		}
-		sit_remove_unstable_phases = remove_unstable_phases;
+		/*sit_remove_unstable_phases = remove_unstable_phases;*/
 		if (sit_remove_unstable_phases == FALSE && mass_water_switch_save == FALSE
 			&& mass_water_switch == TRUE)
 		{
