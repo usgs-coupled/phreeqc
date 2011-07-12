d575975dea580afa0420f92f877fed4aac8a5876
David L Parkhurst <dlpark@usgs.gov>
2011-07-12 23:28:36 +0000

    Removed some header files from phreeqc.h.
    Moved one header to kinetics.c
    
    git-svn-id: svn://136.177.114.72/svn_GW/phreeqc/trunk/src@5475 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- kinetics.c
+++ kinetics.c
@@ -31,11 +31,10 @@
 							 /* CVodeFree, constants OPT_SIZE, BDF, NEWTON,   */
 							 /* SV, SUCCESS, NST,NFE,NSETUPS, NNI, NCFN, NETF */
 #include "cvdense.h"			/* prototype for CVDense, constant DENSE_NJE    */
-#include "nvector_serial.h"		/* definitions of type N_Vector and macro          */
-							 /* NV_Ith_S, prototypes for N_VNew, N_VFree      */
 #include "dense.h"				/* definitions of type DenseMat, macro DENSE_ELEM */
 #endif
-
+#include "nvector_serial.h"		/* definitions of type N_Vector and macro          */
+							 /* NV_Ith_S, prototypes for N_VNew, N_VFree      */
 /* These macros are defined in order to write code which exactly matches
    the mathematical problem description given above.
 
